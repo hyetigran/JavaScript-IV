6cf9d894ee4e84acb9b74ade262523eee31d35b9
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-05-09 15:12:58 +0400

    refactor GameObject

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,12 @@
 // CODE here for your Lambda Classes
+
+class GameObject {
+	constructor(data) {
+		this.createdAt = data.createdAt;
+		this.name = data.name;
+		this.dimensions = data.dimensions;
+	}
+	destroy() {
+		return `${this.name} was removed from the game.`;
+	}
+}
